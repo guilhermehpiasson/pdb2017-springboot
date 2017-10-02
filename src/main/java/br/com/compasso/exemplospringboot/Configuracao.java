f89723792d431e0d7b95f9120bd2ea515dc74a3b
Guilherme Henrique Piasson <guilhermehpiasson@gmail.com>
2017-10-02 15:55:31 -0300

    Alterações de documentação da aplicação

--- src/main/java/br/com/compasso/exemplospringboot/Configuracao.java
+++ src/main/java/br/com/compasso/exemplospringboot/Configuracao.java
@@ -3,6 +3,10 @@ package br.com.compasso.exemplospringboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Classe com a finalidade de executar as configurações da aplicação e deixá-la disponível para o acesso
+ * @author guilherme.piasson
+ */
 @SpringBootApplication
 public class Configuracao {
 	
--- src/main/java/br/com/compasso/exemplospringboot/repository/CartaoRepository.java
+++ src/main/java/br/com/compasso/exemplospringboot/repository/CartaoRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.repository.CrudRepository;
 
 import br.com.compasso.exemplospringboot.model.Cartao;
 
+/**
+ * Interface com o objetivo de promover CRUD da entidade Cartao 
+ * @author guilherme.piasson
+ */
 public interface CartaoRepository extends CrudRepository<Cartao, Long>{
 	
 	
